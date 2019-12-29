4c59c730fe96d91f6a1dd8c2eebf2d355a922036
Andrew Jarombek <ajarombek95@gmail.com>
2019-12-29 16:57:34 -0500

    finish AJNavTextCircle

---
+++ components/test/AJNavTextCircle.snap.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AJNavTextCircle from '../src/AJNavTextCircle/AJNavTextCircle';
+import renderer from 'react-test-renderer';
+
+/**
+ * Snapshot test for the {@link ../../components/AJNavTextCircle} component.
+ * @author Andrew Jarombek
+ * @since 12/29/2019
+ */
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <AJNavTextCircle
+      text="Navigate"
+      textSide="left"
+      height="40px"
+      active={true}
+      direction="right"
+      onClick={f=>f}
+    />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ components/test/AJNavTextCircle.test.js
@@ -0,0 +1,39 @@
+/**
+ * Unit tests with Jest and Enzyme for the {@link ../../components/AJNavTextCircle} component.
+ * @author Andrew Jarombek
+ * @since 12/29/2019
+ */
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import AJNavTextCircle from '../src/AJNavTextCircle/AJNavTextCircle';
+
+describe('unit tests', () => {
+
+  it('renders', () => {
+    const wrapper = shallow(
+      <AJNavTextCircle
+        text="Testing"
+        textSide="left"
+        height="40px"
+        active={true}
+        direction="down"
+        onClick={f => f}
+      />
+    );
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders defaults', () => {
+    const wrapper = shallow(
+      <AJNavTextCircle
+        text="Testing"
+        direction="down"
+        onClick={f => f}
+      />
+    );
+    expect(wrapper.find('div').at(0).hasClass(/(textRight)(-\d+)/)).toBe(true);
+    expect(wrapper.find('div').at(0).childAt(1).type()).toEqual('div');
+    expect(wrapper.find('div').at(0).childAt(1).hasClass(/(description)(-\d+)/)).toBe(true);
+  });
+});
